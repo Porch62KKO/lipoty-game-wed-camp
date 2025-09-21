3fc8d7af13d1d4c64c6ae7dd67f87f3399d16180
Porch62KK0 <korawich12k@gmail.com>
2025-09-21 12:10:45 +0700

    lipoty-game-wed-camp

---
+++ Test.js
@@ -0,0 +1,29 @@
+function PlatGame(){
+
+}
+function UerInput() {
+    userInput = document.getElementById("user-input").va
+    console.log(userInput)
+}
+
+function BotInput() {
+    botInput = Math.florr((Math.random() * 100) %3)
+    console.log(botInput)
+
+}
+
+function CompareBotAndUsreValue() {
+    // ค้อน : 0
+    // กระดาษ : 1
+    // กรรไกร : 2
+    if (userInput == botInput){
+        console.log("DRAW")
+    } else if (userInput == 0 && botInput == 2)
+        // User: ค้อน
+        // BOT: กรรไกร
+
+}
+
+function Odio() {
+
+}
